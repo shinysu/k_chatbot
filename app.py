1ce5aca00ce198ebfd9c5679652cef240bc54eab
shinysu <shiny.suresh@gmail.com>
2021-07-02 12:18:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from google.api_core.exceptions import InvalidArgument
+import dialogflow_v2beta1
+from flask import Flask, render_template, request, jsonify
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
+DIALOGFLOW_PROJECT_ID = 'newagent-jojg'
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+SESSION_ID = 'me'
+
+text_to_be_analyzed = "how does covid spread?"
+
+app = Flask(__name__,
+            template_folder='client/templates',
+            static_folder='client/static')
+
+
+def get_response(text_to_be_analyzed):
+    session_client = dialogflow_v2beta1.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow_v2beta1.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow_v2beta1.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+        return response.query_result.fulfillment_text
+    except InvalidArgument:
+        raise
+
+
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+
+@app.route('/get_reply', methods=["POST"])
+def get_reply():
+    text = request.form["text"]
+    reply = get_response(text)
+    print(reply)
+    return jsonify({'text': reply})
+
+
+'''print("Query text:", response.query_result.query_text)
+print("Detected intent:", response.query_result.intent.display_name)
+print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+print("Fulfillment text:", response.query_result.fulfillment_text)'''
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3001)
---
+++ client/static/js/main.js
@@ -0,0 +1,66 @@
+var Message;
+
+Message = function (arg) {
+		this.text = arg.text, this.message_side = arg.message_side, this.time = arg.time;
+		this.draw = function (_this) {
+				return function () {
+						var $message;
+						$message = $($('.message_template').clone().html());
+						$message.addClass(_this.message_side).find('.text').html(_this.text);
+						$message.addClass(_this.message_side).find('.timestamp').html(_this.time);
+						$('.messages').append($message);
+						return setTimeout(function () {
+								return $message.addClass('appeared');
+						}, 0);
+				};
+		}(this);
+		return this;
+};
+
+function send_message(start = false)
+    {
+      query = {"text":$("#msg_input").val()};
+
+      if (query["text"] === "")
+        return;
+      else{
+        showUserMessage($("#msg_input").val());
+      }
+      $("#msg_input").val("");
+      $.ajax({
+      type: 'POST',
+      url:  "/get_reply" ,
+      data: query,
+      // dataType: 'json',
+      success: function(response) {
+        showBotMessage(response["text"])
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        // alert('error occured');
+      }
+    });
+    }
+
+function showUserMessage(text,init=false){
+    $('#messages').append(
+        ` <div class="chat-message usermessage">
+                ${text}
+            </div>`
+    );
+	if (!init) {
+		$('.messages').animate({ scrollTop: $('.messages').prop('scrollHeight') }, 300);
+	}
+	$('#msg_input').val('');
+}
+
+function showBotMessage(text,init=false){
+    $('#messages').append(
+        `<div class="chat-message botmessage">
+                    ${text}
+                </div>`
+    );
+	if (!init) {
+		$('.messages').animate({ scrollTop: $('.messages').prop('scrollHeight') }, 300);
+	}
+	$('#msg_input').val('');
+}
